47f050d45acc0b1b64caecaf4ff25426082ecf92
Waldyr Filho <dynhoromero@gmail.com>
2023-07-18 19:03:24 -0300

    update: web - nwl 03

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,26 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com carinho,{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://dynhorolf.github.io/DevLinks/"
+        className="underline hover:text-gray-100"
+      >
+        {' '}
+        Waldyr
+      </a>{' '}
+      &{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://rocketseat.com.br"
+      >
+        {' '}
+        Rocketseat
+      </a>
+    </div>
+  )
+}
