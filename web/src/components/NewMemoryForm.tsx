35fafedbc2073028f172d328829253c3d0c1bf72
DynhoROLF <dynhoromero@gmail.com>
2023-08-15 23:52:51 -0300

    fix: file was not uploading. Variable not used

--- web/src/components/NewMemoryForm.tsx
+++ web/src/components/NewMemoryForm.tsx
@@ -79,7 +79,7 @@ export function NewMemoryForm() {
         name="content"
         spellCheck={false}
         className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
-        placeholder="Fique livre para adicionar fotos, videos e relatos sobre essa experiência que você quer lembrar para sempre."
+        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
       />
 
       <button
